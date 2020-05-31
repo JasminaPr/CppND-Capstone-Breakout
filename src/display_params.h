9a01b3802fa287fd7f0fb2b7eaf4c84c5c2ad95a
JasminaPr <jasmina.premec@gmail.com>
2020-05-31 16:59:19 +0000

    Bugfix

--- src/display_params.h
+++ src/display_params.h
@@ -1,6 +1,8 @@
 #ifndef DISPLAYPARAMS_H_
 #define DISPLAYPARAMS_H_
 
+#include <cstddef>
+
 // small struct to hold display parameters for easier passing
 struct DisplayParams{
     std::size_t screen_width;
